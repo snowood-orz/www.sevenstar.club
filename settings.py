fdb0a57e04aeb6835dff70b9e743bab40c1e36d1
Snowwood <772025409@qq.com>
2017-03-21 09:53:29 +0800

    change
    change the path of picture from "/static" to "static"

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "static"),
     "favicon_path": os.path.join(os.path.dirname(__file__), "static"),
     "data_path": os.path.join(os.path.dirname(__file__), "data"),
-    "static_url_prefix": "/static/",
+    "static_url_prefix": "static/",
     "port": 8000,
     "cookie_secret": "sevenstar"
 }
